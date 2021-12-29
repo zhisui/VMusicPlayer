42643213f0b454ff9f9daed9db2b3a279779a951
zhisui <1626676263@qq.com>
2021-12-29 22:11:33 +0800

    feat: new content

---
+++ packages/renderer/src/language/lang/en.js
@@ -0,0 +1,13 @@
+
+export default {
+  nav: {
+    home: 'Home',
+    expore: 'Explore',
+    library: 'Library',
+    search: 'Search',
+    settings: 'Settings',
+    logout: 'Logout',
+    login: 'Login',
+    github: 'Github Repository',
+  },
+};
---
+++ packages/renderer/src/language/lang/zh-CN.js
@@ -0,0 +1,12 @@
+export default {
+  nav: {
+    home: '首页',
+    explore: '发现',
+    library: '音乐库',
+    search: '搜索',
+    settings: '设置',
+    logout: '登录',
+    login: '退出',
+    github: 'Github仓库',
+  },
+};
